25d7b5d3372b318aeae0bf2849ffab7382932789
xtol bruce <xtol@xtols-mac.local>
2016-02-10 13:06:18 +0900

    initial commit : objective-c calculator example

--- ObjCalculator/ViewController.h
+++ ObjCalculator/ViewController.h
@@ -8,8 +8,27 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+typedef enum {
+    STATUS_DEFAULT = 0,
+    STATUS_DIVISION,
+    STATUS_MULTIPLY,
+    STATUS_MINUS,
+    STATUS_PLUS,
+    STATUS_RETURN
+}kStatusCode;
 
 
+@interface ViewController : UIViewController {
+
+double curValue;
+double totalCurValue;
+NSString *curInputValue;
+kStatusCode curStatusCode;
+}
+
+-(IBAction)digitPressed:(UIButton*)sender;
+-(IBAction)operationPressed:(UIButton*)sender;
+
+@property (weak,nonatomic)IBOutlet UILabel *displayLabel;
 @end
 
